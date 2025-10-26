4acccd9508aeb6e8b43055deed6a703585986cb5
Honza S. Copak <bonum.comunae@volny.cz>
2025-10-26 13:53:38 +0100

    Update test_c3mini_ledblink.py

--- test_c3mini_ledblink.py
+++ test_c3mini_ledblink.py
@@ -16,24 +16,20 @@ for blink in range(3):
 
 print("[--- pause ---] 3s")
 sleep_ms(3000)
-# =================================================
+# =================================
 
 from utils.pinout import set_pinout
 from components.led import Led
 
-
 print("[--- simple init ---] pinout")
 pinout = set_pinout()
-
 
 print("[--- simple init ---] led")
 led = Led(pinout.BUILT_IN_LED) # 2
 
 print("[--- test ---] blink 3x")
-for blink in range(3):
-    led.value(0)
-    sleep_ms(500)
-    led.value(1)
+for blink in range(6):
+    led.toggle()
     sleep_ms(500)
 
 print("[-- finish --]\n")
